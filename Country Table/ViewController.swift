7d1de6dd9b05886e97f298825fd0dd5f498ef5f6
MichaHornung <michaelhornung2022@icloud.com>
2022-11-17 10:39:16 +0100

    Initial Commit

---
+++ Country Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Country Table
+//
+//  Created by Michael Hornung on 17.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
